04b1fe0be546f8888636f10bb1229f5f33f9d59a
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-04-26 15:21:51 -0300

    style: set display font and layout

--- br/com/emhk/calc/visao/Display.java
+++ br/com/emhk/calc/visao/Display.java
@@ -1,10 +1,24 @@
 package br.com.emhk.calc.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class Display extends JPanel {
+
+  private final JLabel label;
   
   public Display() {
-  }
+    setBackground(new Color(46, 49, 50));
+    label = new JLabel("1234");
+    label.setForeground(Color.WHITE);
+    label.setFont(new Font("arial", Font.PLAIN, 30));
 
+    setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 20));
+    add(label);
+  }
 }
